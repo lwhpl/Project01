0970148a00d21c730609124184dde10e45eb4746
liwei <liweirr789@126.com>
2019-05-07 23:42:17 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2019/5/7 0007
+  Time: 22:28
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>liwei</title>
+  </head>
+  <body>
+  李大辰
+  </body>
+</html>
---
+++ src/com/liwei/java/HelloWorld.java
@@ -0,0 +1,31 @@
+package com.liwei.java;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("HelloWorld");
+
+        ArrayList al = new ArrayList();//alt+enter手动导包
+
+        System.out.println();
+
+
+        for (int i = 0; i < 1; i++) {
+            
+        }
+
+        for (Object o : al) {
+            
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            
+        }
+    }
+
+
+
+}
